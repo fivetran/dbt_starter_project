8ae83f0a2733410a17ac8b9e2c0ef322905ad22b
Joe Markiewicz <joseph.markiewicz@fivetran.com>
2023-12-01 14:12:57 -0600

    Update package to use extract_url_parameter fivetran_utils macro

--- models/stg_ad_package__url_macro.sql
+++ models/stg_ad_package__url_macro.sql
@@ -1,5 +1,5 @@
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_source') }} as utm_source,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_medium') }} as utm_medium,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_campaign') }} as utm_campaign,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_content') }} as utm_content,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_term') }} as utm_term
+        {{ fivetran_utils.extract_url_parameter('click_uri', 'utm_source') }} as utm_source,
+        {{ fivetran_utils.extract_url_parameter('click_uri', 'utm_medium') }} as utm_medium,
+        {{ fivetran_utils.extract_url_parameter('click_uri', 'utm_campaign') }} as utm_campaign,
+        {{ fivetran_utils.extract_url_parameter('click_uri', 'utm_content') }} as utm_content,
+        {{ fivetran_utils.extract_url_parameter('click_uri', 'utm_term') }} as utm_term
